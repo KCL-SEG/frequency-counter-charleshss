5b8216672cd1ee66957fd0859acd4d43091a4e51
Charles Suddens-Spiers <charlie.suddens-spiers@live.com>
2022-10-03 11:04:06 +0100

    Complete

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,22 @@
 """Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    checked_items = []
+
+    for i in range(len(items)):
+        if str(items[i]) not in checked_items:
+            checked_items.append(str(items[i]))
+
+    for i in range(len(checked_items)):
+        count = 0 #count for each string
+        for j in range(len(items)):
+            if checked_items[i] == str(items[j]):
+                count += 1
+        frequencies[checked_items[i]] = count
+
+    print(checked_items)
+
     return frequencies
+
+print( frequencies([100, 'Hello', '100', '100', 100]) )
